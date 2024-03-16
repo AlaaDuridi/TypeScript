b3c193f50c3209bd2284e3aa0e1513c7188b9a2a
Alaa Duridi <alla_duridi@hotmail.com>
2024-03-17 01:01:37 +0300

    objects_type_aliasing

--- src/objects.ts
+++ src/objects.ts
@@ -11,3 +11,14 @@ let employee: {
 };
 
 // employee.id =0; //this should not be valid, so we add readonly before the id property
+
+
+/**
+ * there is three problems with this objec implementation:
+ * Problem One: if you want to create another employee object, you have to repeat this structure, you'll end up duplicating your code, which is bad
+ * 
+ * Problem two: the other employee object might have other properties, so these two objects might not have a consistent shape because we don't
+ *  have a single place to define the shape of the employee object
+ * 
+ * Problem three:  this structure is making our code hard to understand, this is where we use a type alias, using a tyope alias we can define a custom type
+ * */
