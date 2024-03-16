346a46e12fb1a47a6eea75155c809e0a5452616c
Alaa Duridi <alla_duridi@hotmail.com>
2024-03-16 22:34:58 +0300

    arrays and code completion

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
+/**
+ * Built-In Types
+ */
 let sales = 123_456_789;
 let course = "TypeScript";
 let is_published = true;
  let level ;
-
+ 
+ /**
+  * Any Type
+  */
  level = 1;
  level= "string";
-
  //any type can be in any type, but this is agains the whole idea of using typescript , we used typescript for type safety, for type checking,
  // so if we use the any type, we are not using the typescript as it should be used, so we should avoid using any type as much as possible
 
  //using any type for variable 
-
 
 //  function render(document){ // compilation error: any type can be passed to the function
 //     console.log(document);
@@ -26,3 +30,14 @@ let is_published = true;
 //  function render2(document ){ 
 //     console.log(document);
 //  }
+
+/**
+ * Arrays
+ */
+
+let numbers :number[] = [] ;
+
+numbers[0] =0 ; 
+
+//benifit of typescript: code completion
+numbers.forEach(n=>n.valueOf());
