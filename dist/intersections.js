fa072a1508b13b8909b7e098fba52aa8cfe1d303
Alaa Duridi <alla_duridi@hotmail.com>
2024-03-17 01:12:37 +0300

    intersections

---
+++ dist/intersections.js
@@ -0,0 +1,10 @@
+"use strict";
+let textBox = {
+    drag: () => {
+        console.log("Dragging");
+    },
+    resize: () => {
+        console.log("Resizing");
+    }
+};
+//# sourceMappingURL=intersections.js.map
